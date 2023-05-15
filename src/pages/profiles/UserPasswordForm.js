165c8741bcdbfa20d17a48fe15159ddc75c3b42e
Nici-Shad <61915614+Quack842@users.noreply.github.com>
2023-05-15 08:07:57 +0100

    Added UserPasswordForm and Profile Form details

---
+++ src/pages/profiles/UserPasswordForm.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+
+import {Alert, Button, Container, Row, Col, Form} from "react-bootstrap";
+
+import { useNavigate, useParams } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefault";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+
+import appStyles from "../../App.module.css";
+
+const UserPasswordForm = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const currentUser = useCurrentUser();
+
+  const [userData, setUserData] = useState({
+    new_password1: "",
+    new_password2: "",
+  });
+  const { new_password1, new_password2 } = userData;
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() !== id) {
+      navigate("/");
+    }
+  }, [currentUser, navigate, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.post("/dj-rest-auth/password/change/", userData);
+      // Use -1 instead of history.goBack
+      navigate(-1);
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row>
+      <Col className="py-2 mx-auto text-center" md={6}>
+        <Container className={appStyles.Content}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>New password</Form.Label>
+              <Form.Control
+                placeholder="new password"
+                type="password"
+                value={new_password1}
+                onChange={handleChange}
+                name="new_password1"
+              />
+            </Form.Group>
+            {errors?.new_password1?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Form.Group>
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control
+                placeholder="confirm new password"
+                type="password"
+                value={new_password2}
+                onChange={handleChange}
+                name="new_password2"
+              />
+            </Form.Group>
+            {errors?.new_password2?.map((message, idx) => (
+              <Alert key={idx} variant="warning">
+                {message}
+              </Alert>
+            ))}
+            <Button
+              onClick={() => navigate(-1)}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+            >
+              Save
+            </Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default UserPasswordForm;
